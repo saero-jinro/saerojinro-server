05a49a6e18639b47163d41e3dbffbfff5fd83ac8
fbehddn <tom0611@naver.com>
2025-02-24 17:29:39 +0900

    Initialize project setup

---
+++ src/main/java/com/goorm/saerojinroserver/SaerojinroServerApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.saerojinroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaerojinroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaerojinroServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/saerojinroserver/SaerojinroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.saerojinroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaerojinroServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
