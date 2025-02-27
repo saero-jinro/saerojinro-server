46259c93d8aaf922fbdf640fb9190d2e133fe0ef
minjo-on <alswns11346@gmail.com>
2025-02-27 14:56:48 +0900

    chore: 멀티 모듈 프로젝트 구성

---
+++ saerojinro-api/src/test/java/goorm/saerojinro/api/SaerojinroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.saerojinro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaerojinroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
