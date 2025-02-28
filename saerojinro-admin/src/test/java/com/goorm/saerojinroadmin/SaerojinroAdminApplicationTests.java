481909be3b79735fff28fd1825cbe6f2a979e23c
LeeShinHeang <dragon11796@gmail.com>
2025-02-28 14:14:55 +0900

    chore: admin, speaker 모듈 생성

---
+++ saerojinro-admin/src/test/java/com/goorm/saerojinroadmin/SaerojinroAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.saerojinroadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaerojinroAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
