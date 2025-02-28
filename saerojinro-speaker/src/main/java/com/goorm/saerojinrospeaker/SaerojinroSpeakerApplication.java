fa52e0eb7bb9b0928702bfc34c5665405f5e221d
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2025-02-28 14:23:20 +0900

    chore/#7 admin & speaker 모듈 생성  (#8)

---
+++ saerojinro-speaker/src/main/java/com/goorm/saerojinrospeaker/SaerojinroSpeakerApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.saerojinrospeaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaerojinroSpeakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaerojinroSpeakerApplication.class, args);
+	}
+
+}
