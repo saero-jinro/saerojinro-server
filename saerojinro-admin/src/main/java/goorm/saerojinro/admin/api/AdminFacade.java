a44d063d3ed8da0261fc4063fc7e3e8ee80db944
minjo-on <alswns11346@gmail.com>
2025-02-28 23:08:05 +0900

    chore: db 연동 설정

--- saerojinro-admin/src/main/java/goorm/saerojinro/admin/api/AdminFacade.java
+++ saerojinro-admin/src/main/java/goorm/saerojinro/admin/api/AdminFacade.java
@@ -2,11 +2,24 @@ package goorm.saerojinro.admin.api;
 
 import org.springframework.stereotype.Component;
 
+import goorm.saerojinro.admin.presentation.request.AdminCreateRequest;
+import goorm.saerojinro.admin.presentation.response.AdminPersistResponse;
 import goorm.saerojinro.domain.user.application.UserCommandService;
+import goorm.saerojinro.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class AdminFacade {
 	private final UserCommandService userCommandService;
+
+	public AdminPersistResponse createAdmin(AdminCreateRequest request) {
+		User admin = userCommandService.createAdmin(
+			request.email(), request.password(), request.name()
+		);
+
+		return AdminPersistResponse.from(admin);
+	}
+
+
 }
