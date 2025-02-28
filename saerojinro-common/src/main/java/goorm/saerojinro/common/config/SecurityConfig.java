0726982ef651425bf2a95d51cbbecd05490a1ceb
minjo-on <alswns11346@gmail.com>
2025-02-28 23:13:34 +0900

    feat: securityconfig 일시적으로 permitAll 설정

--- saerojinro-common/src/main/java/goorm/saerojinro/common/config/SecurityConfig.java
+++ saerojinro-common/src/main/java/goorm/saerojinro/common/config/SecurityConfig.java
@@ -2,10 +2,68 @@ package goorm.saerojinro.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+			//.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.logout(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			//.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(SWAGGER_PATTERNS).permitAll()
+				.requestMatchers(STATIC_RESOURCES_PATTERNS).permitAll()
+				.requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
+				.requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+				.anyRequest().permitAll()
+			)
+			.build();
+	}
+
+	private static final String[] SWAGGER_PATTERNS = {
+		"/swagger-ui/**",
+		"/actuator/**",
+		"/v3/api-docs/**",
+	};
+
+	private static final String[] STATIC_RESOURCES_PATTERNS = {
+		"/img/**",
+		"/css/**",
+		"/js/**",
+		"/cloud/**",
+	};
+
+	private static final String[] PERMIT_ALL_PATTERNS = {
+		"/error",
+		"/favicon.ico",
+		"/index.html",
+		"/",
+	};
+
+	private static final String[] PUBLIC_ENDPOINTS = {
+
+	};
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
