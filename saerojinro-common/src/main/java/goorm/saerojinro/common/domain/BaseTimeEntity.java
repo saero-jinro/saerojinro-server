562aa587d9a86585625cdf10c9063e01df81e290
minjo-on <alswns11346@gmail.com>
2025-03-01 11:32:24 +0900

    Update BaseTimeEntity.java

--- saerojinro-common/src/main/java/goorm/saerojinro/common/domain/BaseTimeEntity.java
+++ saerojinro-common/src/main/java/goorm/saerojinro/common/domain/BaseTimeEntity.java
@@ -3,6 +3,7 @@ package goorm.saerojinro.common.domain;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
@@ -17,4 +18,15 @@ public abstract class BaseTimeEntity {
 	@CreationTimestamp
 	@Column(nullable = false, updatable = false)
 	protected LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	protected LocalDateTime updatedAt;
+
+	@Column
+	protected LocalDateTime deletedAt;
+
+	public void delete(){
+		deletedAt = LocalDateTime.now();
+	}
 }
