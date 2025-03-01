66bbe10e2f4ed84d5a6cc3753d6dd5596d8c146f
minjo-on <alswns11346@gmail.com>
2025-03-01 12:06:32 +0900

    refactor: 에러 수정

--- saerojinro-common/src/main/java/goorm/saerojinro/common/exception/ProviderNotFoundException.java
+++ saerojinro-common/src/main/java/goorm/saerojinro/common/exception/ProviderNotFoundException.java
@@ -1,6 +1,6 @@
 package goorm.saerojinro.common.exception;
 
-import static sorisoop.soridam.common.exception.GlobalExceptionCode.PROVIDER_NOT_FOUND;
+import static goorm.saerojinro.common.exception.GlobalExceptionCode.PROVIDER_NOT_FOUND;
 
 public class ProviderNotFoundException extends CustomException {
 	public ProviderNotFoundException() {
