4c16218461ec9994644ebaf969fd4c5e429e0eea
fbehddn <tom0611@naver.com>
2025-03-01 14:50:31 +0900

    chore: 어노테이션 임포트

--- saerojinro-common/src/main/java/goorm/saerojinro/common/domain/BaseTimeEntity.java
+++ saerojinro-common/src/main/java/goorm/saerojinro/common/domain/BaseTimeEntity.java
@@ -2,6 +2,7 @@ package goorm.saerojinro.common.domain;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
