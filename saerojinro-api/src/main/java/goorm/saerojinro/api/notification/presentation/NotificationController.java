5ec84c624eab4f47f5f3e9af7cdbf3a618231f96
LeeShinHeang <dragon11796@gmail.com>
2025-03-04 17:10:44 +0900

    feat: 스웨거 추가

--- saerojinro-api/src/main/java/goorm/saerojinro/api/notification/presentation/NotificationController.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/notification/presentation/NotificationController.java
@@ -4,7 +4,9 @@ import goorm.saerojinro.api.notification.presentation.request.NotificationSendRe
 import goorm.saerojinro.api.notification.presentation.response.ReceivedNotificationListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +21,19 @@ public interface NotificationController {
 			- Description : 이 API는 사용자가 알림을 구독합니다
 			- Assignee : 이신행
 		""")
-	@ApiResponses()
+	@ApiResponse(
+		responseCode = "200",
+		content = @Content(schema = @Schema(implementation = SseEmitter.class))
+	)
 	ResponseEntity<SseEmitter> subscribe();
 
 	@Operation(summary = "알림 조회 API", description = """
 			- Description : 이 API는 사용자가 받은 알림을 조회합니다
 			- Assignee : 이신행
 		""")
-	@ApiResponses()
+	@ApiResponse(
+		responseCode = "200",
+		content = @Content(schema = @Schema(implementation = ReceivedNotificationListResponse.class)))
 	ResponseEntity<ReceivedNotificationListResponse> myNotifications();
 
 	// TODO 추후 Admin 모듈로 분리 예정
@@ -34,7 +41,7 @@ public interface NotificationController {
 			- Description : 이 API는 모든 사용자에게 알림을 전송합니다
 			- Assignee : 이신행
 		""")
-	@ApiResponses()
+	@ApiResponse(responseCode = "204")
 	ResponseEntity<Void> sendAll(
 		@Parameter(
 			description = "전체 알림 request 객체 입니다",
@@ -46,7 +53,7 @@ public interface NotificationController {
 			- Description : 이 API는 강의를 예약한 사용자에게 알림을 전송합니다
 			- Assignee : 이신행
 		""")
-	@ApiResponses()
+	@ApiResponse(responseCode = "204")
 	ResponseEntity<Void> sendByLecture(
 		@PathVariable Long lectureId,
 		@Parameter(
@@ -59,7 +66,7 @@ public interface NotificationController {
 			- Description : 이 API는 특정 사용자에게 알림을 전송합니다
 			- Assignee : 이신행
 		""")
-	@ApiResponses()
+	@ApiResponse(responseCode = "204")
 	ResponseEntity<Void> sendByUserId(
 		@PathVariable Long userId,
 		@Parameter(
