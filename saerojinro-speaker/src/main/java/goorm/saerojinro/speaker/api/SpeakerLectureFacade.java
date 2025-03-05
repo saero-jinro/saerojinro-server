328937636fb85a4f053adecf4da6888b1e56469e
fbehddn <tom0611@naver.com>
2025-03-05 18:58:47 +0900

    refactor: LectureCommandService 의존성 변수명 수정

--- saerojinro-speaker/src/main/java/goorm/saerojinro/speaker/api/SpeakerLectureFacade.java
+++ saerojinro-speaker/src/main/java/goorm/saerojinro/speaker/api/SpeakerLectureFacade.java
@@ -5,6 +5,7 @@ import goorm.saerojinro.domain.lecture.domain.Lecture;
 import goorm.saerojinro.domain.user.application.UserQueryService;
 import goorm.saerojinro.domain.user.domain.User;
 import goorm.saerojinro.speaker.presentation.request.LectureCreateRequest;
+import goorm.saerojinro.speaker.presentation.request.LectureUpdateRequest;
 import goorm.saerojinro.speaker.presentation.response.LectureCreateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,12 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class SpeakerLectureFacade {
-	private final LectureCommandService lectureService;
+	private final LectureCommandService lectureCommandService;
 	private final UserQueryService userQueryService;
 
 	public LectureCreateResponse createLecture(long speakerId, LectureCreateRequest request) {
 		User speaker = userQueryService.getById(speakerId);
-		Lecture lecture = lectureService.createLecture(
+		Lecture lecture = lectureCommandService.createLecture(
 			speaker,
 			request.title(),
 			request.contents(),
