f6fbe4de54c8845d5f471010fc31841737ee13e5
fbehddn <tom0611@naver.com>
2025-03-05 11:00:13 +0900

    refactor: 강의,유저 도메인간 의존성 제거

--- saerojinro-speaker/src/main/java/goorm/saerojinro/speaker/api/SpeakerLectureFacade.java
+++ saerojinro-speaker/src/main/java/goorm/saerojinro/speaker/api/SpeakerLectureFacade.java
@@ -2,6 +2,8 @@ package goorm.saerojinro.speaker.api;
 
 import goorm.saerojinro.domain.lecture.application.LectureCommandService;
 import goorm.saerojinro.domain.lecture.domain.Lecture;
+import goorm.saerojinro.domain.user.application.UserQueryService;
+import goorm.saerojinro.domain.user.domain.User;
 import goorm.saerojinro.speaker.presentation.request.LectureCreateRequest;
 import goorm.saerojinro.speaker.presentation.response.LectureCreateResponse;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SpeakerLectureFacade {
 	private final LectureCommandService lectureService;
+	private final UserQueryService userQueryService;
 
 	public LectureCreateResponse createLecture(long speakerId, LectureCreateRequest request) {
+		User speaker = userQueryService.findById(speakerId);
 		Lecture lecture = lectureService.createLecture(
-			speakerId,
+			speaker,
 			request.title(),
 			request.contents(),
 			request.maxCapacity(),
