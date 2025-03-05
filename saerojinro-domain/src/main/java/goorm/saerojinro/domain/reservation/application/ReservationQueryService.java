923b0d0bffc6b1da0c11752dc4616a4a3eb2999b
fbehddn <tom0611@naver.com>
2025-03-06 00:56:20 +0900

    fix: 예약 서비스 @Getter -> @Service 수정

--- saerojinro-domain/src/main/java/goorm/saerojinro/domain/reservation/application/ReservationQueryService.java
+++ saerojinro-domain/src/main/java/goorm/saerojinro/domain/reservation/application/ReservationQueryService.java
@@ -5,12 +5,12 @@ import goorm.saerojinro.domain.reservation.domain.Reservation;
 import goorm.saerojinro.domain.reservation.domain.ReservationRepository;
 import goorm.saerojinro.domain.reservation.exception.ReservationNotFoundException;
 import goorm.saerojinro.domain.user.domain.User;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Getter
+@Service
 @RequiredArgsConstructor
 public class ReservationQueryService {
     
