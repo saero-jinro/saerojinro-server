2f4a07049b87036df7dbef9630c916302d74e0bf
fbehddn <tom0611@naver.com>
2025-03-06 00:55:47 +0900

    refactor: 강연자 스웨거, 구현 컨트롤러 분리

--- saerojinro-speaker/src/main/java/goorm/saerojinro/speaker/presentation/SpeakerController.java
+++ saerojinro-speaker/src/main/java/goorm/saerojinro/speaker/presentation/SpeakerController.java
@@ -1,30 +1,42 @@
 package goorm.saerojinro.speaker.presentation;
 
-import goorm.saerojinro.speaker.api.SpeakerLectureFacade;
 import goorm.saerojinro.speaker.presentation.request.LectureCreateRequest;
 import goorm.saerojinro.speaker.presentation.request.LectureUpdateRequest;
 import goorm.saerojinro.speaker.presentation.response.LectureCreateResponse;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@RestController
-@RequestMapping("/api/speakers")
-@RequiredArgsConstructor
-public class SpeakerController {
-	private final SpeakerLectureFacade speakerLectureFacade;
+@Tag(name = "Speaker", description = "강연자 API")
+public interface SpeakerController {
+	@Operation(
+		summary = "강의 생성",
+		description = "강연자가 강의를 생성합니다.",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				content = @Content(schema = @Schema(implementation = LectureCreateResponse.class))
+			)
+		}
+	)
+	ResponseEntity<LectureCreateResponse> create(@PathVariable(name = "id") Long speakerId,
+														@RequestBody LectureCreateRequest request);
 
-	@PostMapping("/{id}/lectures")
-	private ResponseEntity<LectureCreateResponse> createLecture(@PathVariable(name = "id") Long speakerId,
-																@RequestBody LectureCreateRequest request) {
-		return ResponseEntity.ok(speakerLectureFacade.createLecture(speakerId, request));
-	}
-
-	@PatchMapping("/{id}/lectures/{lectureId}")
-	private ResponseEntity<Void> updateLecture(@PathVariable(name = "id") Long speakerId,
-											   @PathVariable Long lectureId,
-											   @RequestBody LectureUpdateRequest request) {
-		speakerLectureFacade.updateLecture(speakerId, lectureId, request);
-		return ResponseEntity.noContent().build();
-	}
+	@Operation(
+		summary = "강의 수정",
+		description = "강연자 혹은 관리자가 강의를 수정합니다",
+		responses = {
+			@ApiResponse(
+				responseCode = "200"
+			)
+		}
+	)
+	ResponseEntity<Void> update(@PathVariable(name = "id") Long speakerId,
+									   @PathVariable Long lectureId,
+									   @RequestBody LectureUpdateRequest request);
 }
