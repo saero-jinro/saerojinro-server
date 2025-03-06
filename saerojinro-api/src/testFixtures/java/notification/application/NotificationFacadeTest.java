68db7b9c07b1b98d026f807e5ff0994d0f52d31b
LeeShinHeang <dragon11796@gmail.com>
2025-03-06 09:46:27 +0900

    refactor: 권한 분리를 위한 SseSender 클래스 추가

--- saerojinro-api/src/testFixtures/java/notification/application/NotificationFacadeTest.java
+++ saerojinro-api/src/testFixtures/java/notification/application/NotificationFacadeTest.java
@@ -15,6 +15,7 @@ import goorm.saerojinro.domain.reservation.domain.ReservationRepository;
 import goorm.saerojinro.domain.user.application.UserQueryService;
 import goorm.saerojinro.domain.user.domain.User;
 import goorm.saerojinro.domain.user.domain.UserRepository;
+import goorm.saerojinro.infra.notification.sse.NotificationSseSender;
 import goorm.saerojinro.infra.repository.impl.EmitterRepositoryImpl;
 import mock.repository.FakeNotificationRepository;
 import mock.repository.FakeReservationRepository;
@@ -39,7 +40,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class NotificationFacadeTest {
 	private NotificationFacade notificationFacade;
 	private NotificationRepository repository;
-	private UserRepository userRepository;
 	private ReservationRepository reservationRepository;
 
 	private User user;
@@ -55,14 +55,16 @@ public class NotificationFacadeTest {
 		EmitterRepository emitterRepository = new EmitterRepositoryImpl();
 
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		userRepository = new FakeUserRepository();
+		UserRepository userRepository = new FakeUserRepository();
 		UserQueryService userQueryService = new UserQueryService(userRepository, passwordEncoder);
 
 		reservationRepository = new FakeReservationRepository();
 		ReservationQueryService reservationQueryService = new ReservationQueryService(reservationRepository);
 
+		NotificationSseSender sseSender = new NotificationSseSender();
+
 		notificationFacade = new NotificationFacade(
-			commandService, queryService, emitterRepository, userQueryService, reservationQueryService);
+			commandService, queryService, emitterRepository, userQueryService, reservationQueryService, sseSender);
 
 		user = userRepository.save(User.builder()
 			.email("email@email.com")
@@ -135,27 +137,6 @@ public class NotificationFacadeTest {
 		Assertions.assertEquals(CONTENTS, result.getContents());
 	}
 
-	@Test
-	@DisplayName("sendNotification는 알림을 전송한다")
-	void sendNotification_Success() {
-		// given
-		notificationFacade.subscribe();
-		Long receiverId = 1L;
-		Notification notification = Notification.createNotification(
-			userRepository.findById(1L).get(), TITLE, CONTENTS
-		);
-
-		// when
-		notificationFacade.sendNotification(receiverId, notification);
-
-		// then
-		List<Notification> notifications = repository.findByUserId(receiverId);
-		Notification result = notifications.get(0);
-		assertNotNull(notifications);
-		Assertions.assertEquals(TITLE, result.getTitle());
-		Assertions.assertEquals(CONTENTS, result.getContents());
-	}
-
 
 	@Test
 	@DisplayName("sendNotificationAll는 모든 참가자에게 알림을 전송한다")
@@ -184,10 +165,11 @@ public class NotificationFacadeTest {
 		// given
 		notificationFacade.subscribe();
 		Long receiverId = 1L;
-		Notification notification = Notification.createNotification(
-			userRepository.findById(1L).get(), TITLE, CONTENTS
-		);
-		notificationFacade.sendNotification(receiverId, notification);
+		NotificationSendRequest notification = NotificationSendRequest.builder()
+			.title(TITLE)
+			.contents(CONTENTS)
+			.build();
+		notificationFacade.sendNotificationByReceiverId(receiverId, notification);
 
 		// when
 		ReceivedNotificationListResponse response = notificationFacade.myNotification();
