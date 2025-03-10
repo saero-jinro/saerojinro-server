31dce871558741570af451fe6f090511d6fd0f1f
fbehddn <tom0611@naver.com>
2025-03-10 15:05:30 +0900

    refactor: 강연자 생성 로직 변경

--- saerojinro-admin/src/main/java/goorm/saerojinro/admin/lecture/presentation/request/LectureCreateRequest.java
+++ saerojinro-admin/src/main/java/goorm/saerojinro/admin/lecture/presentation/request/LectureCreateRequest.java
@@ -29,7 +29,21 @@ public record LectureCreateRequest(
 	@NotNull String location,
 
 	@Schema(description = "강의 카테고리", example = "BACKEND", requiredMode = Schema.RequiredMode.REQUIRED)
-	@NotNull Category category
+	@NotNull Category category,
 
-	// 강연자 필드만 할당할지 생성할지
+	// 강연자
+	@Schema(description = "강연자 이메일", example = "google@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String speakerEmail,
+
+	@Schema(description = "강연자 회사, 직급", example = "블라블라 스타트업 CEO", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String speakerPosition,
+
+	@Schema(description = "한 줄 소개", example = "안녕하세요 OO 기업에서 OO를 담당하는 OOO 입니다.", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String speakerIntroduction,
+
+	@Schema(description = "상세 약력", example = "OO 회사 / 프로젝트 OO 담당", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String speakerFilmography,
+
+	@Schema(description = "강연자 사진", example = "강연자 사진 URI", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull String speakerPhoto
 ) {}
--- saerojinro-domain/src/main/java/goorm/saerojinro/domain/lecture/application/LectureCommandService.java
+++ saerojinro-domain/src/main/java/goorm/saerojinro/domain/lecture/application/LectureCommandService.java
@@ -4,7 +4,7 @@ import goorm.saerojinro.common.domain.Category;
 import goorm.saerojinro.domain.lecture.domain.Lecture;
 import goorm.saerojinro.domain.lecture.domain.LectureRepository;
 import goorm.saerojinro.domain.lecture.exception.LectureNotFoundException;
-import goorm.saerojinro.domain.user.domain.User;
+import goorm.saerojinro.domain.speaker.domain.Speaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +16,10 @@ import java.time.LocalDateTime;
 public class LectureCommandService {
 	private final LectureRepository lectureRepository;
 
-	public Lecture create(User speaker, String title, String contents, Long maxCapacity,
+	public Lecture create(Speaker speaker, String title, String contents, Long maxCapacity,
 						  LocalDateTime startTime, LocalDateTime endTime, String location, Category category) {
 
-		Lecture lecture = Lecture.create(null, title, contents, maxCapacity, startTime, endTime, location, category);
+		Lecture lecture = Lecture.create(speaker,title, contents, maxCapacity, startTime, endTime, location, category);
 		return lectureRepository.save(lecture);
 	}
 
