958bc3a5cbc8f8368d0e4b2e0ffa20a01d2ec202
LeeShinHeang <dragon11796@gmail.com>
2025-03-11 13:36:41 +0900

    refactor: 반환 객체 스웨거 추가

--- saerojinro-api/src/main/java/goorm/saerojinro/api/timetable/presentation/response/TimetableResponse.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/timetable/presentation/response/TimetableResponse.java
@@ -1,12 +1,42 @@
 package goorm.saerojinro.api.timetable.presentation.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.List;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @Builder
 public record TimetableResponse(
+	@Schema(description = "예약 리스트",
+		example = "[{"
+			+ "\"reservationId\": 1, "
+			+ "\"userId\": 1, "
+			+ "\"lectureId\": 1, "
+			+ "\"title\": \"클린코드란\", "
+			+ "\"startTime\": \"2025-03-01T10:00:00\", "
+			+ "\"endTime\": \"2025-03-01T12:00:00\", "
+			+ "\"currentReservation\": 1, "
+			+ "\"capacity\": 80, "
+			+ "\"location\": \"101호\", "
+			+ "\"speakerName\": \"마틴 파울러\"}]",
+		requiredMode = REQUIRED)
 	List<ReservationListResponse> reservation,
+
+	@Schema(description = "즐겨 찾기 리스트",
+		example = "[{"
+			+ "\"wishlistId\": 1, "
+			+ "\"userId\": 1, "
+			+ "\"lectureId\": 1, "
+			+ "\"title\": \"클린코드란\", "
+			+ "\"startTime\": \"2025-03-01T10:00:00\", "
+			+ "\"endTime\": \"2025-03-01T12:00:00\", "
+			+ "\"currentReservation\": 1, "
+			+ "\"capacity\": 80, "
+			+ "\"location\": \"101호\", "
+			+ "\"speakerName\": \"마틴 파울러\"}]",
+		requiredMode = REQUIRED)
 	List<WishlistListResponse> wishlist
 ) {
 	public static TimetableResponse of(List<ReservationListResponse> reservation, List<WishlistListResponse> wishlist) {
--- saerojinro-api/src/main/java/goorm/saerojinro/api/timetable/presentation/response/WishlistListResponse.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/timetable/presentation/response/WishlistListResponse.java
@@ -2,21 +2,43 @@ package goorm.saerojinro.api.timetable.presentation.response;
 
 import goorm.saerojinro.domain.lecture.domain.Lecture;
 import goorm.saerojinro.domain.wishlist.domain.WishList;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 @Builder
 public record WishlistListResponse(
+	@Schema(description = "즐겨찾기 id", example = "1", requiredMode = REQUIRED)
 	Long wishlistId,
+
+	@Schema(description = "참가자 id", example = "1", requiredMode = REQUIRED)
 	Long userId,
+
+	@Schema(description = "강의 id", example = "1", requiredMode = REQUIRED)
 	Long lectureId,
+
+	@Schema(description = "강의 제목", example = "클린코드란", requiredMode = REQUIRED)
 	String title,
+
+	@Schema(description = "시작시간", example = "2025-03-01T10:00:00", requiredMode = REQUIRED)
 	LocalDateTime startTime,
+
+	@Schema(description = "종료시간", example = "2025-03-01T12:00:00", requiredMode = REQUIRED)
 	LocalDateTime endTime,
+
+	@Schema(description = "현재 예약 인원", example = "1", requiredMode = REQUIRED)
 	int currentReservation,
+
+	@Schema(description = "예약 가능 인원", example = "80", requiredMode = REQUIRED)
 	int capacity,
+
+	@Schema(description = "위치", example = "101호", requiredMode = REQUIRED)
 	String location,
+
+	@Schema(description = "강연자 이름", example = "마틴 파울러", requiredMode = REQUIRED)
 	String speakerName
 ) {
 	public static WishlistListResponse from(WishList wishList, int currentReservation) {
