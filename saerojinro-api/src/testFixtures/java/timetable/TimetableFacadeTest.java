a84dd460fc999e2e3867f92a3b9e25914b34ed61
LeeShinHeang <dragon11796@gmail.com>
2025-03-11 11:49:44 +0900

    test: 테스트 리팩토링

--- saerojinro-api/src/testFixtures/java/timetable/TimetableFacadeTest.java
+++ saerojinro-api/src/testFixtures/java/timetable/TimetableFacadeTest.java
@@ -101,7 +101,7 @@ public class TimetableFacadeTest {
 		assertEquals(LocalDateTime.parse("2025-01-01T00:00:00"), timetable.reservation().get(0).startTime());
 		assertEquals(LocalDateTime.parse("2025-01-01T01:00:00"), timetable.reservation().get(0).endTime());
 		assertEquals(1, timetable.wishlist().size());
-		assertEquals(10, timetable.wishlist().get(0).capacity());
+		assertEquals(8, timetable.wishlist().get(0).capacity());
 		assertEquals("Room 1", timetable.wishlist().get(0).location());
 		assertEquals("콜 팔머", timetable.wishlist().get(0).speakerName());
 	}
