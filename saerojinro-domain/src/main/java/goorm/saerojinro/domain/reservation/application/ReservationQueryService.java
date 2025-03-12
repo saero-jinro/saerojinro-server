14c39f50c39589f091740fbb80629f3ef730ec82
SD-gif <alem6516@naver.com>
2025-03-12 10:45:59 +0900

    refactor: 충돌제거

--- saerojinro-domain/src/main/java/goorm/saerojinro/domain/reservation/application/ReservationQueryService.java
+++ saerojinro-domain/src/main/java/goorm/saerojinro/domain/reservation/application/ReservationQueryService.java
@@ -38,15 +38,8 @@ public class ReservationQueryService {
         }
     }
 
-
     public int countByLectureId(Long lectureId) {
         return reservationRepository.countByLectureId(lectureId);
-    }
-
-    public void validateReservationByUserAndStartTime(Long userId, LocalDateTime startTime) {
-        if (existsCheckByUserAndStartTime(userId, startTime)) {
-            throw new ReservationExistException();
-        }
     }
 
 }
