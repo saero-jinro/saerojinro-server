9082da09cd053f9b290954f7095153b6c9ec045f
minjo-on <alswns11346@gmail.com>
2025-03-12 17:55:49 +0900

    feat: 카테고리 가중치 계산 후 리스트 추출 구현

--- saerojinro-domain/src/main/java/goorm/saerojinro/domain/lecture/application/LectureRecommendationService.java
+++ saerojinro-domain/src/main/java/goorm/saerojinro/domain/lecture/application/LectureRecommendationService.java
@@ -1,8 +1,14 @@
 package goorm.saerojinro.domain.lecture.application;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import goorm.saerojinro.common.domain.Category;
 import goorm.saerojinro.domain.lecture.domain.LectureRepository;
+import goorm.saerojinro.domain.logevent.domain.LogEvent;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -10,5 +16,13 @@ import lombok.RequiredArgsConstructor;
 public class LectureRecommendationService {
 	private final LectureRepository lectureRepository;
 
-
+	public List<Category> getRecommendationCategories(List<LogEvent> logEvents) {
+		return logEvents.stream()
+			.collect(Collectors.groupingBy(LogEvent::getCategory,
+				Collectors.summingInt(logEvent -> logEvent.getLogEventType().getWeight())))
+			.entrySet().stream()
+			.sorted(Map.Entry.<Category, Integer>comparingByValue().reversed())
+			.map(Map.Entry::getKey)
+			.toList();
+	}
 }
