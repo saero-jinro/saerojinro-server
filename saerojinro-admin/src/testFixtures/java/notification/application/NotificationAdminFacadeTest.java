68294008827c0e57fa098f6477fa928f5edbe320
SD-gif <alem6516@naver.com>
2025-03-12 10:27:46 +0900

    refactor: Reservation 생성 메소드 명 변경에 따른 리팩터링

--- saerojinro-admin/src/testFixtures/java/notification/application/NotificationAdminFacadeTest.java
+++ saerojinro-admin/src/testFixtures/java/notification/application/NotificationAdminFacadeTest.java
@@ -85,7 +85,7 @@ public class NotificationAdminFacadeTest {
 	void sendNotificationByLectureId_Success() {
 		// given
 		Lecture lecture = Lecture.builder().id(1L).build();
-		reservationRepository.save(Reservation.createReservation(user, lecture));
+		reservationRepository.save(Reservation.create(user, lecture));
 
 		NotificationSendRequest request = NotificationSendRequest.builder()
 			.title(TITLE)
