5a13d1e946da554785c46e11f7fa11fd22a2eb34
SeoDong <79890834+SD-gif@users.noreply.github.com>
2025-03-12 11:17:23 +0900

    feature/#81 예약 등록 검증 로직 변경 (#90)

--- saerojinro-domain/src/main/java/goorm/saerojinro/domain/reservation/domain/ReservationRepository.java
+++ saerojinro-domain/src/main/java/goorm/saerojinro/domain/reservation/domain/ReservationRepository.java
@@ -1,18 +1,16 @@
 package goorm.saerojinro.domain.reservation.domain;
 
-import goorm.saerojinro.domain.lecture.domain.Lecture;
-import goorm.saerojinro.domain.user.domain.User;
-
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository {
 
-    boolean existByUserAndLecture(User user, Lecture lecture);
+    boolean existByUserIdAndStartTime(Long userId, LocalDateTime startTime);
 
-    List<Reservation> findByUser(User user);
+    List<Reservation> findByUserId(Long userId);
 
-    Optional<Reservation> findByUserAndLecture(User user, Lecture lecture);
+    Optional<Reservation> findByUserIdAndLectureId(Long userId, Long lectureId);
 
     Reservation save(Reservation reservation);
 
--- saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/impl/ReservationRepositoryImpl.java
+++ saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/impl/ReservationRepositoryImpl.java
@@ -1,13 +1,12 @@
 package goorm.saerojinro.infra.repository.impl;
 
-import goorm.saerojinro.domain.lecture.domain.Lecture;
 import goorm.saerojinro.domain.reservation.domain.Reservation;
 import goorm.saerojinro.domain.reservation.domain.ReservationRepository;
-import goorm.saerojinro.domain.user.domain.User;
 import goorm.saerojinro.infra.repository.jpa.ReservationJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,18 +16,18 @@ public class ReservationRepositoryImpl implements ReservationRepository {
     private final ReservationJpaRepository reservationJpaRepository;
 
     @Override
-    public boolean existByUserAndLecture(User user, Lecture lecture) {
-        return reservationJpaRepository.existsByUserAndLecture(user, lecture);
+    public boolean existByUserIdAndStartTime(Long userId, LocalDateTime startTime) {
+        return reservationJpaRepository.existsByUserIdAndStartTime(userId, startTime);
     }
 
     @Override
-    public List<Reservation> findByUser(User user) {
-        return reservationJpaRepository.findAllByUser(user);
+    public List<Reservation> findByUserId(Long userId) {
+        return reservationJpaRepository.findAllByUserId(userId);
     }
 
     @Override
-    public Optional<Reservation> findByUserAndLecture(User user, Lecture lecture) {
-        return reservationJpaRepository.findByUserAndLecture(user, lecture);
+    public Optional<Reservation> findByUserIdAndLectureId(Long userId, Long lectureId) {
+        return reservationJpaRepository.findByUserIdAndLectureId(userId, lectureId);
     }
 
     @Override
