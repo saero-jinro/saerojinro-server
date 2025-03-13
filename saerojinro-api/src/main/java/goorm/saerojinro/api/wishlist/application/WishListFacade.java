c530f39bab529c6d47d4e13415ac0922df6d6d23
SD-gif <alem6516@naver.com>
2025-03-13 13:48:50 +0900

    feat: 즐겨찾기 시간대별 조회 기능 구현
    # Conflicts:
    #	saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/jpa/WishListJpaRepository.java

--- saerojinro-api/src/main/java/goorm/saerojinro/api/wishlist/application/WishListFacade.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/wishlist/application/WishListFacade.java
@@ -1,7 +1,6 @@
 package goorm.saerojinro.api.wishlist.application;
 
 import goorm.saerojinro.api.wishlist.presentation.response.WishListCreateResponse;
-import goorm.saerojinro.api.wishlist.presentation.response.WishListDetailResponse;
 import goorm.saerojinro.api.wishlist.presentation.response.WishListResponse;
 import goorm.saerojinro.domain.lecture.application.LectureQueryService;
 import goorm.saerojinro.domain.lecture.domain.Lecture;
@@ -14,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -28,6 +28,13 @@ public class WishListFacade {
 	public WishListResponse getAllWishList() {
 		User user = userQueryService.me();
 		List<WishList> wishLists = wishListQueryService.getAllByUser(user);
+		return WishListResponse.from(wishLists);
+	}
+
+	@Transactional(readOnly = true)
+	public WishListResponse getByUserAndStartTime(LocalDateTime startTime){
+		User user = userQueryService.me();
+		List<WishList> wishLists = wishListQueryService.getByUserAndStartTime(user, startTime);
 		return WishListResponse.from(wishLists);
 	}
 
