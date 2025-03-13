d3a36629aecbfb2b2dcd14469ee2564d9aa1aca7
minjo-on <alswns11346@gmail.com>
2025-03-13 12:56:34 +0900

    test: LogEventServiceTest 작성

--- saerojinro-domain/src/testFixtures/java/logevent/application/LogEventServiceTest.java
+++ saerojinro-domain/src/testFixtures/java/logevent/application/LogEventServiceTest.java
@@ -6,11 +6,16 @@ import static goorm.saerojinro.domain.logevent.domain.enums.LogEventType.LECTURE
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import goorm.saerojinro.domain.file.domain.File;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import goorm.saerojinro.common.domain.Category;
@@ -65,6 +70,12 @@ public class LogEventServiceTest {
 			.build()
 		);
 
+		UserDetails user = userQueryService.getByEmail("email@email.com");
+		SecurityContext context = SecurityContextHolder.getContext();
+		context.setAuthentication(
+			new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities())
+		);
+
 		lecture = fakeLectureRepository.save(Lecture.create(
 			Speaker.builder().build(),
 			"Lecture One",
@@ -94,5 +105,24 @@ public class LogEventServiceTest {
 		assertEquals(logEventDto.category(), response.getCategory());
 		assertEquals(user, response.getUser());
 		assertEquals(lecture, response.getLecture());
+	}
+
+	@Test
+	@DisplayName("getLogEventByUser는 해당 유저의 EventLog를 조회한다.")
+	public void getLogEventsByUser_Success() {
+		// given
+		String record = "record";
+		LogEventDto logEventDto1 = LogEventDto.of(1L, 1L, LECTURE_RESERVATION_SUCCESS, BACKEND);
+		LogEventDto logEventDto2 = LogEventDto.of(1L, 1L, LECTURE_RESERVATION_SUCCESS, BACKEND);
+		LogEventDto logEventDto3 = LogEventDto.of(1L, 1L, LECTURE_RESERVATION_SUCCESS, BACKEND);
+
+		logEventService.save(record + 1, logEventDto1);
+		logEventService.save(record + 2, logEventDto2);
+		logEventService.save(record + 3, logEventDto3);
+
+		// when
+		List<LogEvent> response = logEventService.getLogEventsByUser();
+		// then
+		assertEquals(3, response.size());
 	}
 }
