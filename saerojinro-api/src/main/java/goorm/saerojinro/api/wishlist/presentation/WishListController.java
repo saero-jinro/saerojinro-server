c00c6b89950b15195952b695dce5bdbdec7b84a2
LeeShinHeang <dragon11796@gmail.com>
2025-03-17 10:34:52 +0900

    test: getByUserAndStartTime 테스트 수정

--- saerojinro-api/src/main/java/goorm/saerojinro/api/wishlist/presentation/WishListController.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/wishlist/presentation/WishListController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
--- saerojinro-api/src/main/java/goorm/saerojinro/api/wishlist/presentation/WishListControllerImpl.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/wishlist/presentation/WishListControllerImpl.java
@@ -1,17 +1,22 @@
 package goorm.saerojinro.api.wishlist.presentation;
 
 import goorm.saerojinro.api.lecture.presentation.response.LectureSummaryListResponse;
-import goorm.saerojinro.api.wishlist.application.*;
+import goorm.saerojinro.api.wishlist.application.WishListFacade;
 import goorm.saerojinro.api.wishlist.presentation.response.WishListCreateResponse;
 import goorm.saerojinro.api.wishlist.presentation.response.WishListResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
