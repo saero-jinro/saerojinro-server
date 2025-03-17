e641fd51b98b04c2bb48ce19226e080558c3f9cd
minjo-on <alswns11346@gmail.com>
2025-03-17 11:50:46 +0900

    feat: 마이페이지 조회 api 구현

---
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/user/presentation/response/UserInfoResponse.java
@@ -0,0 +1,23 @@
+package goorm.saerojinro.api.user.presentation.response;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import goorm.saerojinro.domain.user.domain.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+public record UserInfoResponse(
+	@Schema(description = "사용지 이름", example = "박민준", requiredMode = REQUIRED)
+	String name,
+
+	@Schema(description = "사용자 이메일", example = "alswns11346@kgu.ac.kr", requiredMode = REQUIRED)
+	String email
+) {
+	public static UserInfoResponse from(User user) {
+		return UserInfoResponse.builder()
+			.name(user.getName())
+			.email(user.getEmail())
+			.build();
+	}
+}
