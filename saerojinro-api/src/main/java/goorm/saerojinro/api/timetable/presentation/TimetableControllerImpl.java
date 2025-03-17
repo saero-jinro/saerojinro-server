ea72840fac2b5d8bc980b60d791fb21d71ae5a79
minjo-on <alswns11346@gmail.com>
2025-03-17 13:59:35 +0900

    Update TimetableControllerImpl.java

--- saerojinro-api/src/main/java/goorm/saerojinro/api/timetable/presentation/TimetableControllerImpl.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/timetable/presentation/TimetableControllerImpl.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/timetable")
+@RequestMapping("/api/timetables")
 public class TimetableControllerImpl implements TimetableController {
 
 	private final TimetableFacade timetableFacade;
