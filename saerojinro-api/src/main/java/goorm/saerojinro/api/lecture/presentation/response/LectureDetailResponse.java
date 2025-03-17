a71b8872b6384c71201a8e4c3f19433b3e94bb5f
fbehddn <tom0611@naver.com>
2025-03-17 17:43:28 +0900

    refactor: 강의 세부 정보 응답 수정

--- saerojinro-api/src/main/java/goorm/saerojinro/api/lecture/presentation/response/LectureDetailResponse.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/lecture/presentation/response/LectureDetailResponse.java
@@ -11,18 +11,6 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Builder
 public record LectureDetailResponse(
-	@Schema(description = "강의명", example = "공간지능 혁신을 통한 온오프라인 통합 경험의 미래", requiredMode = REQUIRED)
-	String title,
-
-	@Schema(description = "강의 내용", example = "Part 1: 온오프라인 경험을 연결하는 네이버 지도가 공간지능과 만나 제공하게 될 미래 모습을 소개합니다.", requiredMode = REQUIRED)
-	String contents,
-
-	@Schema(description = "강의 자료 파일 ID", example = "3")
-	Long materialsId,
-
-	@Schema(description = "강의 카테고리", example = "BACKEND", requiredMode = REQUIRED)
-	Category category,
-
 	@Schema(description = "강의 시작 시간", example = "2025-03-01T10:00:00", requiredMode = REQUIRED)
 	LocalDateTime startTime,
 
@@ -30,17 +18,47 @@ public record LectureDetailResponse(
 	LocalDateTime endTime,
 
 	@Schema(description = "강의 장소", example = "온라인", requiredMode = REQUIRED)
-	String location
+	String location,
+
+	@Schema(description = "강의명", example = "공간지능 혁신을 통한 온오프라인 통합 경험의 미래", requiredMode = REQUIRED)
+	String title,
+
+	@Schema(description = "강의 카테고리", example = "BACKEND", requiredMode = REQUIRED)
+	Category category,
+
+	@Schema(description = "강의 내용", example = "이 강의는 ~ 만들 수 있습니다", requiredMode = REQUIRED)
+	String contents,
+
+	@Schema(description = "강의 자료 파일 ID", example = "3", requiredMode = REQUIRED)
+	Long materialsId,
+
+	@Schema(description = "강연자 이름", example = "박민수", requiredMode = REQUIRED)
+	String speakerName,
+
+	@Schema(description = "강연자 이메일", example = "google@google.com", requiredMode = REQUIRED)
+	String speakerEmail,
+
+	@Schema(description = "강연자 소개", example = "안녕하세요 박민수입니다", requiredMode = REQUIRED)
+	String introduction,
+
+	@Schema(description = "강연자 사진 url", example = "bucketUrl/uploads/{lectureId}/speaker", requiredMode = REQUIRED)
+	String speakerPhotoUrl
+
+
 ) {
 	public static LectureDetailResponse from(Lecture lecture) {
 		return LectureDetailResponse.builder()
-			.title(lecture.getTitle())
-			.contents(lecture.getContents())
-			.materialsId(lecture.getMaterialFile().getId())
-			.category(lecture.getCategory())
 			.startTime(lecture.getStartTime())
 			.endTime(lecture.getEndTime())
 			.location(lecture.getLocation())
+			.title(lecture.getTitle())
+			.category(lecture.getCategory())
+			.contents(lecture.getContents())
+			.materialsId(lecture.getMaterialFile().getId())
+			.speakerName(lecture.getSpeaker().getName())
+			.speakerEmail(lecture.getSpeaker().getEmail())
+			.introduction(lecture.getSpeaker().getIntroduction())
+			.speakerPhotoUrl(lecture.getSpeaker().getImageFile().getPhysicalPath())
 			.build();
 	}
 }
