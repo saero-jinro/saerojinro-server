6883b68d6986ce83fb8f0f32f75fd4ce868eda6d
minjo-on <alswns11346@gmail.com>
2025-03-17 13:26:00 +0900

    refactor: 질문 Swagger 예시 추가

--- saerojinro-api/src/main/java/goorm/saerojinro/api/question/presentation/response/QuestionCreateResponse.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/question/presentation/response/QuestionCreateResponse.java
@@ -1,10 +1,14 @@
 package goorm.saerojinro.api.question.presentation.response;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record QuestionCreateResponse(
-        Long id
+    @Schema(description = "질문 ID", example = "1L", requiredMode = REQUIRED)
+    Long id
 ) {
     public static QuestionCreateResponse from(Long questionsId){
         return QuestionCreateResponse.builder()
