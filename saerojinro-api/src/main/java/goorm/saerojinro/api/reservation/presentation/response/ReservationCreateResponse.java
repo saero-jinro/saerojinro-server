0e272f1547833b79b307388dd9b0cdefe0beacd5
minjo-on <alswns11346@gmail.com>
2025-03-17 14:45:02 +0900

    Update ReservationCreateResponse.java

--- saerojinro-api/src/main/java/goorm/saerojinro/api/reservation/presentation/response/ReservationCreateResponse.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/reservation/presentation/response/ReservationCreateResponse.java
@@ -1,12 +1,16 @@
 package goorm.saerojinro.api.reservation.presentation.response;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 import goorm.saerojinro.domain.reservation.domain.Reservation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 public record ReservationCreateResponse(
-        Long id
+    @Schema(description = "예약 ID", example = "1", requiredMode = REQUIRED)
+    Long id
 ){
     public static ReservationCreateResponse from(Reservation reservation){
         return ReservationCreateResponse.builder()
