9a2e4021ba6ed32776f3cef2aa0dc6b2e0c20a85
LeeShinHeang <dragon11796@gmail.com>
2025-03-20 09:48:55 +0900

    fix: LocalDateTime Json 직렬화, 역직렬화 추가

--- saerojinro-common/src/main/java/goorm/saerojinro/common/domain/BaseTimeEntity.java
+++ saerojinro-common/src/main/java/goorm/saerojinro/common/domain/BaseTimeEntity.java
@@ -6,6 +6,12 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -15,16 +21,24 @@ import lombok.Getter;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-
 	@CreationTimestamp
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonFormat(pattern = "yyyy-mm-dd kk:mm:ss")
 	@Column(nullable = false, updatable = false)
 	protected LocalDateTime createdAt;
 
 	@UpdateTimestamp
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonFormat(pattern = "yyyy-mm-dd kk:mm:ss")
 	@Column(nullable = false)
 	protected LocalDateTime updatedAt;
 
 	@Column
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonFormat(pattern = "yyyy-mm-dd kk:mm:ss")
 	protected LocalDateTime deletedAt;
 
 	public void delete(){
