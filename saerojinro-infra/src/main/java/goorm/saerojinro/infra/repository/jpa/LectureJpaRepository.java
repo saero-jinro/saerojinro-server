f40c0f99c59dbbb85424cc4dc40fbd9c5df5944a
Dongwoo You <108855080+fbehddn@users.noreply.github.com>
2025-03-21 15:54:35 +0900

    fix/#154 삭제된 강의 조회 제외 및 강의 DDL 수정 (#155)

--- saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/jpa/LectureJpaRepository.java
+++ saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/jpa/LectureJpaRepository.java
@@ -14,23 +14,20 @@ import java.util.Optional;
 
 public interface LectureJpaRepository extends JpaRepository<Lecture, Long> {
 	@Query("SELECT l FROM Lecture l JOIN FETCH l.speaker " +
-		"WHERE l.startTime >= :start AND l.startTime < :end")
+		"WHERE l.startTime >= :start AND l.startTime < :end AND l.deletedAt IS NULL")
 	List<Lecture> findByStartTimeBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
-	List<Lecture> findAll();
+	List<Lecture> findAllByDeletedAtIsNull();
 
-	Optional<Lecture> findById(@Param("id") Long id);
+	Optional<Lecture> findByIdAndDeletedAtIsNull(@Param("id") Long id);
 
 	@Lock(LockModeType.PESSIMISTIC_READ)
-	@Query("SELECT l FROM Lecture l WHERE l.id = :id")
+	@Query("SELECT l FROM Lecture l WHERE l.id = :id AND l.deletedAt IS NULL")
 	Optional<Lecture> findByIdWithLock(@Param("id") Long id);
 
-	List<Lecture> findByStartTime(LocalDateTime time);
+	List<Lecture> findByStartTimeAndDeletedAtIsNull(LocalDateTime time);
 
-	List<Lecture> findByStartTimeGreaterThanEqualAndEndTimeLessThanEqual(LocalDateTime startTime, LocalDateTime endTime);
+	List<Lecture> findByStartTimeGreaterThanEqualAndEndTimeLessThanEqualAndDeletedAtIsNull(LocalDateTime startTime, LocalDateTime endTime);
 
-	List<Lecture> findByCategoryInAndStartTime(List<Category> categories, LocalDateTime lectureTime);
-
-	@Query("SELECT l FROM Lecture l JOIN FETCH l.speaker s WHERE s.id = :speakerId")
-	Optional<Lecture> findBySpeakerId(@Param("speakerId") Long speakerId);
+	List<Lecture> findByCategoryInAndStartTimeAndDeletedAtIsNull(List<Category> categories, LocalDateTime lectureTime);
 }
