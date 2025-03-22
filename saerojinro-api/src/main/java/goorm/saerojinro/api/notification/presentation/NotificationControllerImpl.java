b1b6f9d17aea281f9639abb13aa582af64ee3ff5
LeeShinHeang <dragon11796@gmail.com>
2025-03-22 16:17:12 +0900

    refactor: SseEmitter GetMapping으로 변경

--- saerojinro-api/src/main/java/goorm/saerojinro/api/notification/presentation/NotificationControllerImpl.java
+++ saerojinro-api/src/main/java/goorm/saerojinro/api/notification/presentation/NotificationControllerImpl.java
@@ -5,7 +5,6 @@ import goorm.saerojinro.api.notification.presentation.response.ReceivedNotificat
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -17,7 +16,7 @@ public class NotificationControllerImpl implements NotificationController {
 	private final NotificationFacade notificationFacade;
 
 	@Override
-	@PostMapping("/subscribe")
+	@GetMapping("/subscribe")
 	public ResponseEntity<SseEmitter> subscribe() {
 		SseEmitter response = notificationFacade.subscribe();
 		return ResponseEntity.ok(response);
