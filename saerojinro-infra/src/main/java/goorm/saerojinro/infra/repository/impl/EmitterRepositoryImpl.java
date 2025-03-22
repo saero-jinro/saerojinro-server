a1354bab9e25dce013e40ff39718c3b32cfab4e8
LeeShinHeang <dragon11796@gmail.com>
2025-03-22 16:23:38 +0900

    refactor: SseEmitter save 이미 존재할 경우 저장된 emitter 리턴

--- saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/impl/EmitterRepositoryImpl.java
+++ saerojinro-infra/src/main/java/goorm/saerojinro/infra/repository/impl/EmitterRepositoryImpl.java
@@ -2,9 +2,9 @@ package goorm.saerojinro.infra.repository.impl;
 
 import goorm.saerojinro.domain.notification.domain.EmitterRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -14,13 +14,21 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public class EmitterRepositoryImpl implements EmitterRepository {
 	private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+	public static long TIMEOUT = 12 * 60 * 60 * 1000L;
 
 	@Override
 	public SseEmitter save(Long id) {
-		Optional<SseEmitter> emitter = findById(id);
-		emitter.ifPresent(ResponseBodyEmitter::complete);
+		Optional<SseEmitter> existingEmitter = findById(id);
+		if (existingEmitter.isPresent()) {
+			try {
+				existingEmitter.get().send(SseEmitter.event());
+				return existingEmitter.get();
+			} catch (IOException e) {
+				existingEmitter.get().complete();
+			}
+		}
 
-		SseEmitter newEmitter = new SseEmitter(12 * 60 * 60 * 1000L);
+		SseEmitter newEmitter = new SseEmitter(TIMEOUT);
 		newEmitter.onCompletion(() -> emitters.remove(id));
 		newEmitter.onTimeout(newEmitter::complete);
 		emitters.put(id, newEmitter);
