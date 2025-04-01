45fa1e2c1a03a2e2e7cdba3a4215362317c83353
LeeShinHeang <dragon11796@gmail.com>
2025-04-01 15:39:33 +0900

    refactor: common 모듈 정리

--- saerojinro-common/src/main/java/goorm/saerojinro/common/domain/blacklist/exception/BlackListedTokenException.java
+++ saerojinro-common/src/main/java/goorm/saerojinro/common/domain/blacklist/exception/BlackListedTokenException.java
@@ -1,7 +1,6 @@
 package goorm.saerojinro.common.domain.blacklist.exception;
 
 import static goorm.saerojinro.common.domain.blacklist.exception.BlackListExceptionCode.BLACK_LISTED_TOKEN;
-import static goorm.saerojinro.common.domain.reissue.exception.ReissueExceptionCode.REFRESH_TOKEN_NOT_FOUND;
 
 import goorm.saerojinro.common.exception.CustomException;
 
